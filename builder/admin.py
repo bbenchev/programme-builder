dbfc9295407a8bd5988e6a44256046f901c6b9fa
Boris Benchev <boris.benchev@bbc.co.uk>
2018-02-25 18:46:44 +0000

    add accreditation and criteria models

--- builder/admin.py
+++ builder/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Module)
+admin.site.register(Programme)
+admin.site.register(Accreditation)
+admin.site.register(Criterion)
--- builder/models.py
+++ builder/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
 class Module(models.Model):
+    COMPULSORY = "C"
+    OPTIONAL = "O"
+    DISCOVERY = "D"
+    MODULE_TYPES = (
+        (COMPULSORY, "Compulsory"),
+        (OPTIONAL, "Optional"),
+        (DISCOVERY, "Discovery")
+    )
     title = models.CharField(max_length=100)
     code = models.CharField(max_length=15)
     credits = models.CharField(max_length=2)
     taught_semester = models.CharField(max_length=1)
-    module_type = models.CharField(max_length=30)
+    module_type = models.CharField(max_length=1, choices=MODULE_TYPES,
+        default=COMPULSORY, blank=False)
     level = models.CharField(max_length=1)
+    prerequisites = models.ManyToManyField('self', symmetrical=False,
+        related_name='is_prerequisite_of')
 
     def __str__(self):
         return self.title
@@ -14,7 +25,24 @@ class Module(models.Model):
 class Programme(models.Model):
     name = models.CharField(max_length=100)
     ucas_code = models.CharField(max_length=10)
-    modules = models.ManyToManyField(Module)
+    years = models.CharField(max_length=1, default=3)
+    modules = models.ManyToManyField(Module, related_name='programmes')
+
+    def __str__(self):
+        return self.name
+
+
+class Criterion(models.Model):
+    code = models.CharField(max_length=10)
+    definition = models.TextField()
+
+    def __str__(self):
+        return self.code
+
+
+class Accreditation(models.Model):
+    name = models.CharField(max_length=20)
+    criteria = models.ManyToManyField(Criterion, related_name='accreditations')
 
     def __str__(self):
         return self.name
