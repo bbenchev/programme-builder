dea74aad86880abdff9fd638e3ff18a320d474f5
Boris Benchev <boris.benchev@bbc.co.uk>
2018-03-18 18:13:53 +0000

    create simple forms for signing up and loging in, integrated with the User model

--- builder/urls.py
+++ builder/urls.py
@@ -3,8 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.login_handler, name="index"),
     path('home/', views.home, name="home"),
+    path('signup/', views.signup_handler, name="signup"),
+    path('logout/', views.logout_handler, name="logout"),
     path('modules/', views.get_modules, name="modules"),
     path('modules/<int:id>/', views.module),
     path('build/', views.build, name="build"),
--- builder/views.py
+++ builder/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
-from .models import Module
-from django.contrib.auth import authenticate
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Module
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
-def index(request):
-    return render(request, "login.html", {})
 
+
+def login_handler(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            if "next" in request.POST:
+                return redirect(request.POST.get("next"))
+            else:
+                return redirect("home")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "index.html", {"form": form})
+
+@login_required(login_url="/")
+def home(request):
+    return render(request, "home.html", {})
 
 def get_modules(request):
     context = {"modules": Module.objects.all()}
@@ -17,12 +36,23 @@ def module(request,id):
 
     return render(request, "module.html", context)
 
+@login_required(login_url="/")
 def build(request):
     return render(request, "build.html", {})
 
-def home(request):
-    user = authenticate(username=request.POST["uname"], password=request.POST["password"])
-    if user is not None:
-        return render(request, "home.html", {})
+def signup_handler(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
     else:
-        return HttpResponse("boris")
+        form = UserCreationForm()
+    return render(request, "signup.html", {'form': form})
+
+def logout_handler(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("index")
+
